b1fe318e9b71f1ce616c3629bf533f175cc4d0f7
PrashantIndurkar <prashantri2000@gmail.com>
2022-03-05 22:46:25 +0530

    completed heading component with css

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,8 +1,43 @@
 import React from "react";
 import "./HomeScreen.css";
-
+import { Fade } from "react-awesome-reveal";
+import { Link } from "react-router-dom";
+import Heading from "../Heading";
 function HomeScreen() {
-	return <div></div>;
+	return (
+		<div className="homeScreen">
+			<Fade>
+				<div className="homeScreen_top">
+					<h4>
+						We’re working hard to put the health and well-being of our partners
+						and customers first in all that we do. <Link>Learn more</Link>
+					</h4>
+				</div>
+			</Fade>
+			<Fade>
+				<div className="homeScreen_bottom">
+					<div className="homeScreen_bottomLeft">
+						<h4>Investing in our communities</h4>
+						<p>
+							The Starbucks Foundation awarded grants to over 400 nonprofits
+							serving communities of color. Recipients were nominated by
+							Starbucks partners (employees).
+						</p>
+						<button>Learn more</button>
+					</div>
+					<div className="homeScreen_bottomRight">
+						<img
+							src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-68026.jpg"
+							alt=""
+						/>
+					</div>
+				</div>
+			</Fade>
+			<Fade>
+				<Heading heading="TODAY IS YOURS" />
+			</Fade>
+		</div>
+	);
 }
 
 export default HomeScreen;
--- src/SignUpButton.js
+++ src/SignUpButton.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SignUpButton.css";
 
 export default function SignUpButton() {
-	return <div>
-		
-	</div>;
+	return (
+		<Link className="signUpButton" to="/account/create">
+			Join Now
+		</Link>
+	);
 }
